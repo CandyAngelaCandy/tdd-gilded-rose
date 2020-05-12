5f6a8342c0d0454eb43f83502bd5e451c1475ce4
Yanyan Huang <yyhuang@thoughworks.com>
2020-05-12 21:41:11 +0800

    [linxiao/yanyan] add test about case 2

--- src/test/java/cn/xpbootcamp/gildedrose/GoodsTest.java
+++ src/test/java/cn/xpbootcamp/gildedrose/GoodsTest.java
@@ -11,4 +11,12 @@ public class GoodsTest {
         assertEquals(milk.getSellIn(), 5);
         assertEquals(milk.getQuantity(), 15);
     }
+
+    @Test
+    public void should_get_sellIn_is_negative_1_quantity_is_8_when_past_11_days_given_a_normal_goods_with_sellIn_is_10_quantity_is_20() {
+        Goods milk = new Goods("N001", "milk", GoodsType.NORMAL, 10, 20);
+        milk.passDays(11);
+        assertEquals(milk.getSellIn(), -1);
+        assertEquals(milk.getQuantity(), 8);
+    }
 }
